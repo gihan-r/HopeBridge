afa8bc7f0a96f4091d43952dd01377205f19e898
gihan-r <kavimcg03@gmail.com>
2025-03-23 15:15:13 +0530

    Update GitHub Pages configuration

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/HopeBridge',
+  basePath: process.env.NODE_ENV === 'production' ? '/HopeBridge' : '',
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig 
